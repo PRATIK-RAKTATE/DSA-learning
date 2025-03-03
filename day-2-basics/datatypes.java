0501b01f2f28f6698e1e0bb23af09c63d5dd2761
Pratik Raktate <rhutikraktate@gmail.com>
2025-03-03 19:22:32 +0530

    Datatypes in java

---
+++ day-2-basics/datatypes.java
@@ -0,0 +1,22 @@
+public class datatypes {
+    public static void main(String[] args) {
+        // two types 
+        // primitive and non primitive
+
+//         // primitive 
+//         byte	1 byte	Stores whole numbers from -128 to 127
+// short	2 bytes	Stores whole numbers from -32,768 to 32,767
+// char	2 bytes	Stores a single character (Unicode)
+// boolean	1 byte (depends on JVM)	Stores true or false
+// int	4 bytes	Stores whole numbers from -2³¹ to 2³¹-1
+// long	8 bytes	Stores whole numbers from -2⁶³ to 2⁶³-1
+// float	4 bytes	Stores decimal numbers (single precision)
+// double	8 bytes	Stores decimal numbers (double precision)
+
+// String	Varies	Stores a sequence of characters (depends on content)
+// Array	Varies	Stores multiple values of the same type (depends on elements)
+// Class	Varies	Blueprint for creating objects (size depends on attributes and methods)
+// Object	Varies	Parent class of all Java classes (depends on fields/methods)
+// Interface	Varies	Defines a contract for classes (size depends on methods implemented)
+    }
+}
