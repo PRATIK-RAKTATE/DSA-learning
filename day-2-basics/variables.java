4bea3e3f6d3d9c1e2b256ca7a072dd08b5536805
Pratik Raktate <rhutikraktate@gmail.com>
2025-03-03 19:17:50 +0530

    Variables

---
+++ day-2-basics/variables.java
@@ -0,0 +1,11 @@
+public class variables {
+    public static void main(String[] args) {
+        int a = 2;
+        int b = 3;
+        String s = "pratik";
+
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(s);
+    }
+}
