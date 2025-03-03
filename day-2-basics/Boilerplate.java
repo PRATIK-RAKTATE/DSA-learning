12b0fbb80dc78e8bf329b10031e6fea443237533
Pratik Raktate <rhutikraktate@gmail.com>
2025-03-03 09:32:23 +0530

    Boilerplate code in java

---
+++ day-2-basics/Boilerplate.java
@@ -0,0 +1,12 @@
+class Boilerplate {
+    public static void main(String args[]) {
+        // print() print and further output is in same line
+        System.out.print("hello world"); 
+
+        // println() prints in next line
+        System.out.println("hello world");
+
+        // After \n prints in the next line
+        System.out.print("hello world\n");
+    }
+}
